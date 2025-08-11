4b4abb9c3cd459febdc2d11e55b3914796454e37
Neeraj6197 <neerajjain6197@gmail.com>
2025-08-11 21:21:17 +0530

    fifth commit

--- iris_demo.py
+++ iris_demo.py
@@ -20,7 +20,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Train a Random Forest Classifier
 params = {'n_estimators': 300,
-            'max_depth': 25}
+            'max_depth': 20}
 
 model = RandomForestClassifier(**params)
 model.fit(X_train, y_train)
